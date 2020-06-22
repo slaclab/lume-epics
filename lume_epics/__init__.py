c37f5a0a8b9edc6aba1cd9635a6b10880d248235
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-06-22 12:14:54 -0400

    MNT: Temp fix for missing introspections

--- lume_epics/__init__.py
+++ lume_epics/__init__.py
@@ -1,5 +1,33 @@
 WIDGET_LIST = ["striptool", "image", "table", "slider"]
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
+
+
+from lume_model.variables import (
+    ScalarInputVariable,
+    ScalarOutputVariable,
+    ImageInputVariable,
+    ImageOutputVariable,
+)
+
+INPUT_VARIABLE_TYPES = (
+    ScalarInputVariable,
+    ImageInputVariable,
+)
+OUTPUT_VARIABLE_TYPES = (
+    ScalarOutputVariable,
+    ImageOutputVariable,
+)
+
+IMAGE_VARIABLE_TYPES = (
+    ImageInputVariable,
+    ImageOutputVariable,
+)
+
+SCALAR_VARIABLE_TYPES = (
+    ScalarInputVariable,
+    ScalarOutputVariable,
+)
