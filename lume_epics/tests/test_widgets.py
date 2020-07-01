c417fcdb0b64e63b4e92364cfe83076f0d768e56
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-07-01 15:29:43 -0400

    MNT: Configure client tests

--- lume_epics/tests/test_widgets.py
+++ lume_epics/tests/test_widgets.py
@@ -10,31 +10,73 @@ from lume_epics.client.widgets.tables import ValueTable
 from lume_epics.client.widgets.sliders import build_sliders
 from lume_epics.client.widgets.plots import ImagePlot, Striptool
 from lume_epics.client.controller import Controller
+from lume_epics import epics_server
+from lume_model.models import SurrogateModel
+
+
+class ExampleModel(SurrogateModel):
+    input_variables = {
+        "input1": ScalarInputVariable(name="input1", value=1, range=[0.0, 5.0]),
+        "input2": ScalarInputVariable(name="input2", value=2, range=[0.0, 5.0]),
+    }
+
+    output_variables = {
+        "output1": ScalarOutputVariable(name="output1"),
+        "output2": ScalarOutputVariable(name="output2"),
+        "output3": ImageOutputVariable(
+            name="output3",
+            value=np.array([[1, 2,], [3, 4]]),
+            axis_labels=["count_1", "count_2"],
+            x_min=0,
+            y_min=0,
+            x_max=5,
+            y_max=5,
+        ),
+    }
+
+    def evaluate(self, input_variables):
+
+        self.input_variables = {variable.name: variable for variable in input_variables}
+
+        self.output_variables["output1"].value = (
+            self.input_variables["input1"].value * 2
+        )
+        self.output_variables["output2"].value = (
+            self.input_variables["input2"].value * 2
+        )
+
+        # return inputs * 2
+        return list(self.output_variables.values())
 
 
 def test_sliders():
-    input1 = ScalarInputVariable(name="input1", value=1, range=[0.0, 5.0])
-    input2 = ScalarInputVariable(name="input2", value=2, range=[0.0, 5.0])
-
-    inputs = [input1, input2]
-    PROTOCOL = "pva"
     PREFIX = "test"
 
+    server = epics_server.Server(ExampleModel, PREFIX)
+    server.start(monitor=False)
+
+    inputs = list(ExampleModel.input_variables.values())
+
     # create controller
-    controller = Controller(PROTOCOL)
+    controller = Controller("pva")
 
     # build sliders for the command process variable database
     sliders = build_sliders(inputs, controller, PREFIX)
 
     controller.close()
+    server.stop()
 
 
 def test_value_table():
+    PROTOCOL = "pva"
+    PREFIX = "test"
+
+    server = epics_server.Server(ExampleModel, PREFIX)
+    server.start(monitor=False)
+
     output1 = ScalarOutputVariable(name="output1")
     output2 = ScalarOutputVariable(name="output2")
 
-    PROTOCOL = "pva"
-    PREFIX = "test"
     # create controller
     controller = Controller(PROTOCOL)
 
@@ -43,48 +85,35 @@ def test_value_table():
     value_table = ValueTable(outputs, controller, PREFIX)
 
     controller.close()
+    server.stop()
 
 
 def test_image_plot():
-    output1 = ImageOutputVariable(
-        name="output1",
-        value=np.array([[1, 2,], [3, 4]]),
-        axis_labels=["count_1", "count_2"],
-        x_min=0,
-        y_min=0,
-        x_max=5,
-        y_max=5,
-    )
-
-    output2 = ImageOutputVariable(
-        name="output2",
-        value=np.array([[1, 6,], [4, 1]]),
-        axis_labels=["count_1", "count_2"],
-        x_min=0,
-        y_min=0,
-        x_max=5,
-        y_max=5,
-    )
-
     PROTOCOL = "pva"
     PREFIX = "test"
 
+    server = epics_server.Server(ExampleModel, PREFIX)
+    server.start(monitor=False)
     # create controller
     controller = Controller(PROTOCOL)
 
-    outputs = [output1, output2]
-
+    outputs = [ExampleModel.output_variables["output3"]]
     value_table = ImagePlot(outputs, controller, PREFIX)
 
     controller.close()
+    server.stop()
 
 
 def test_striptool():
+    PROTOCOL = "pva"
+    PREFIX = "test"
+
+    server = epics_server.Server(ExampleModel, PREFIX)
+    server.start(monitor=False)
+
     output1 = ScalarOutputVariable(name="output1")
     output2 = ScalarOutputVariable(name="output2")
 
-    PROTOCOL = "pva"
-    PREFIX = "test"
     # create controller
     controller = Controller(PROTOCOL)
 
@@ -93,3 +122,4 @@ def test_striptool():
     value_table = Striptool(outputs, controller, PREFIX)
 
     controller.close()
+    server.stop()
