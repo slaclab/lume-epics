d02bb956cdeac390bae0ac0a729ddee2d2125b9d
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-09-22 14:10:57 -0700

    WIP: Implement EntryTable widget

--- lume_epics/client/widgets/controls.py
+++ lume_epics/client/widgets/controls.py
@@ -8,7 +8,8 @@ from functools import partial
 from typing import Union, List
 import logging
 
-from bokeh.models import Slider
+from bokeh.models import Slider, ColumnDataSource, DataTable, TableColumn, StringFormatter, StringEditor, Button
+from bokeh.events import Tap
 
 from lume_model.variables import ScalarInputVariable
 from lume_epics.client.controller import Controller
@@ -114,3 +115,110 @@ def set_pv_from_slider(
 
     """
     controller.put(pvname, new)
+
+
+class EntryTable:
+    """
+    Table of process variable names and values.
+
+    Attriibutes:
+        pv_monitors (Dict[str, PVScalar]): Monitors associated with process variables.
+
+        output_values (dict): Dict mapping process variable name to current value.
+
+        labels (dict): Dict mapping process variable name to labels.
+
+        source (ColumnDataSource): Data source for populating bokeh table.
+
+
+
+    """
+    def __init__(
+        self, variables: List[ScalarInputVariable], controller: Controller, prefix: str,
+    ) -> None:
+        """
+        Initialize table.
+
+        Args:
+            variables (List[ScalarVariable]): List of variables to display.
+
+            controller (Controller): Controller object for accessing process variables.
+
+            prefix (str): Prefix used in setting up the server.
+
+        """
+        self.output_values = {}
+        self.labels = {}
+        self.prefix = prefix
+        self.controller = controller
+
+        # be sure to surface units in the table
+        self.unit_map = {}
+
+        for variable in variables:
+            self.output_values[variable.name] = ""
+
+            # check if units assigned
+            if "units" in variable.__fields_set__ and variable.units:
+                self.labels[variable.name] = variable.name + f" ({variable.units})"
+
+            else:
+                self.labels[variable.name] = variable.name
+
+
+        self.clear_button = Button(label="Clear")
+        self.clear_button.on_click(self.clear)
+        self.submit_button = Button(label="Submit")
+        self.submit_button.on_click(self.submit)
+        self.create_table()
+
+    def create_table(self) -> None:
+        """
+        Creates the bokeh table and populates variable data.
+        """
+        x_vals = [self.labels[var] for var in self.output_values.keys()]
+        y_vals = list(self.output_values.values())
+        table_data = dict(x=x_vals, values =y_vals)
+
+        columns = [
+            TableColumn(
+                field="x", title="Outputs", formatter=StringFormatter(font_style="bold")
+            ),
+            TableColumn(field = "values", title = "Value", editor = StringEditor())
+        ]
+
+        self.source = ColumnDataSource(table_data)
+        self.source.on_change('data', self.update_values)
+
+        self.table = DataTable(
+            source=self.source, columns=columns, editable=True, selectable=True, sortable=False, index_position=None, auto_edit=True
+        )
+
+    def submit(self) -> None:
+        """
+        Function to submit values entered into table
+        """
+        print("putting...")
+
+        for variable in self.output_values:
+            if self.output_values[variable] != "":
+                pvname = f"{self.prefix}:{variable}"
+                self.controller.put(pvname, self.output_values[variable])
+
+    def clear(self) -> None:
+        """
+        Function to clear all entered values
+        """
+        for variable in self.output_values:
+            self.output_values[variable] = ""
+
+        x_vals = [self.labels[var] for var in self.output_values.keys()]
+        y_vals = list(self.output_values.values())
+
+        self.source.data = dict(x=x_vals, values=y_vals)
+
+
+    def update_values(self, attr, new, old):
+
+        for i, label in enumerate(new["x"]):
+            self.output_values[label] = new["values"][i]
