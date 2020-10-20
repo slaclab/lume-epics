73e316e1393ee2c571bc17783997668215d7ecd5
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-10-20 12:50:23 -0700

    MNT: Get path directly from import

--- lume_epics/commands/render_from_template.py
+++ lume_epics/commands/render_from_template.py
@@ -1,5 +1,6 @@
 import click
 import subprocess
+from lume_epics.commands import bokeh_template
 
 @click.command()
 @click.argument("filename")
@@ -7,7 +8,8 @@ import subprocess
 @click.argument("prefix")
 @click.option("--read-only", default=False, type=bool)
 def render_from_template(filename, protocol, prefix, read_only):
-    subprocess.call(["bokeh", "serve", "lume_epics/commands/bokeh_template.py", "--show", "--args", filename, protocol, prefix, str(read_only)])
+    template_file = bokeh_template.__file__
+    subprocess.call(["bokeh", "serve", template_file, "--show", "--args", filename, protocol, prefix, str(read_only)])
 
 
 if __name__ == "__main__":
