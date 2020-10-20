49dae8eab15f2bd2cdaa59ca10e41fa8b5af6d0d
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-10-20 11:10:40 -0700

    MNT: Format commands

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     python_requires=">=3.7",
     entry_points={
         "console_scripts": [
-        "renderfromtemplate=bin.render_from_template:render_from_template",
-        "servefromtemplate=bin.serve_from_template:serve_from_template"]
+        "render-from-template=bin.render_from_template:render_from_template",
+        "serve-from-template=bin.serve_from_template:serve_from_template"]
     },
 )
