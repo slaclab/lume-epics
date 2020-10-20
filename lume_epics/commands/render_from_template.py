4c8ced6a67c3b888e908d6053de5f3968ee1a27f
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-10-20 12:48:36 -0700

    MNT: Correct template path

--- lume_epics/commands/render_from_template.py
+++ lume_epics/commands/render_from_template.py
@@ -7,7 +7,7 @@ import subprocess
 @click.argument("prefix")
 @click.option("--read-only", default=False, type=bool)
 def render_from_template(filename, protocol, prefix, read_only):
-    subprocess.call(["bokeh", "serve", "bin/bokeh_template.py", "--show", "--args", filename, protocol, prefix, str(read_only)])
+    subprocess.call(["bokeh", "serve", "lume_epics/commands/bokeh_template.py", "--show", "--args", filename, protocol, prefix, str(read_only)])
 
 
 if __name__ == "__main__":
