0f67222ca9d5ef592b7286d2febe2d6b9b4f5a6e
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-10-20 20:34:13 -0700

    MNT: Remove protocol from serving command

--- lume_epics/commands/serve_from_template.py
+++ lume_epics/commands/serve_from_template.py
@@ -5,11 +5,10 @@ from lume_model.utils import model_from_yaml
 
 @click.command()
 @click.argument("filename")
-@click.argument("protocol")
 @click.argument("prefix")
 @click.option("--serve-ca", type=bool, default=True)
 @click.option("--serve-pva", type=bool, default=True)
-def serve_from_template(filename, protocol, prefix, serve_ca, serve_pva):
+def serve_from_template(filename, prefix, serve_ca, serve_pva):
 
     with open(filename, "r") as f:
         model_class, model_kwargs = model_from_yaml(f, load_model=False)
