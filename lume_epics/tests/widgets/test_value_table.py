cb915ccb8d904edabd072af7c4a9bef518cd3a10
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-12-09 14:38:45 -0800

    TST: Update value table inputs/outputs

--- lume_epics/tests/widgets/test_value_table.py
+++ lume_epics/tests/widgets/test_value_table.py
@@ -2,32 +2,41 @@ import pytest
 import epics
 from lume_epics.client.widgets.tables import ValueTable
 
+
+@pytest.fixture(scope="module")
+def input_variables(model):
+    return [
+        var for var in model.input_variables.values() if var.variable_type == "scalar"
+    ]
+
+
 @pytest.fixture(scope="module")
 def table_variables(model):
-    return [var for var in model.output_variables.values() if var.variable_type == "scalar"]
+    return [
+        var for var in model.output_variables.values() if var.variable_type == "scalar"
+    ]
+
 
 @pytest.fixture(scope="module")
 def value_table(controller, model, prefix, table_variables):
 
     return ValueTable(table_variables, controller, prefix)
 
-@pytest.mark.skip(reason="Requires controller bug fix.")
-def test_value_table_update(value_table, prefix, table_variables):
 
-    updated_vals = {}
+@pytest.mark.parametrize("value", [(1), (5), (8)])
+def test_value_table_update(
+    value, value_table, prefix, table_variables, server, input_variables
+):
 
-    for var in table_variables:
-        val_idx = value_table._source.data["x"].index(var.name)
-        val = value_table._source.data["y"][val_idx]
-
-        updated_vals[var.name] = float(val) * 2
-        epics.caput(f"{prefix}:{var.name}", float(val) * 2)
+    # update input variables to trigger output update
+    for var in input_variables:
+        epics.caput(f"{prefix}:{var.name}", value)
 
     value_table.update()
 
     for var in table_variables:
         val_idx = value_table._source.data["x"].index(var.name)
+        epics_val = epics.caget(f"{prefix}:{var.name}")
         val = value_table._source.data["y"][val_idx]
 
-        assert updated_vals[var.name] == float(val)
-    
+        assert epics_val == float(val)
