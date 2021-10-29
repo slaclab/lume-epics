20d490b2be383c915f98344c7cd25eae4231b033
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-10-29 13:04:00 -0700

    Check in read-only

---
+++ examples/read_only/server.py
@@ -0,0 +1,20 @@
+from lume_epics.epics_server import Server
+from lume_model.utils import variables_from_yaml
+import os
+os.environ["PTYHONPATH"] = "/Users/jgarra/sandbox/lume-epics"
+from examples.model import DemoModel
+
+with open("examples/files/demo_config.yml", "r") as f:
+    input_variables, output_variables = variables_from_yaml(f)
+
+prefix = "test"
+server = Server(
+    DemoModel,
+    prefix,
+    model_kwargs={"input_variables": input_variables, "output_variables": output_variables},
+    read_only=True,
+    protocols=["pva"]
+
+)
+# monitor = False does not loop in main thread
+server.start(monitor=True)
