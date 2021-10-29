c58c62b0bb56884d71f2be36be0187d8284ffaa5
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-10-29 13:04:07 -0700

    Check in read-only

--- examples/read_only/model.py
+++ examples/read_only/model.py
@@ -1,7 +1,12 @@
 import numpy as np
-from lume_model.variables import ScalarInputVariable, ImageOutputVariable, ScalarOutputVariable
+from lume_model.variables import (
+    ScalarInputVariable,
+    ImageOutputVariable,
+    ScalarOutputVariable,
+)
 from lume_model.models import SurrogateModel
 from lume_model.utils import save_variables
+
 
 class DemoModel(SurrogateModel):
     def __init__(self, input_variables=None, output_variables=None):
@@ -11,9 +16,9 @@ class DemoModel(SurrogateModel):
     def evaluate(self, input_variables):
         input_variables = {input_var.name: input_var for input_var in input_variables}
         self.output_variables["output1"].value = np.random.uniform(
-            input_variables["input1"].value, # lower dist bound
-            input_variables["input2"].value, # upper dist bound
-            (50,50)
+            input_variables["input1"].value,  # lower dist bound
+            input_variables["input2"].value,  # upper dist bound
+            (50, 50),
         )
         self.output_variables["output2"].value = input_variables["input1"].value
         self.output_variables["output3"].value = input_variables["input2"].value
--- examples/read_only/server.py
+++ examples/read_only/server.py
@@ -1,6 +1,7 @@
 from lume_epics.epics_server import Server
 from lume_model.utils import variables_from_yaml
 import os
+
 os.environ["PTYHONPATH"] = "/Users/jgarra/sandbox/lume-epics"
 from examples.model import DemoModel
 
@@ -11,10 +12,12 @@ prefix = "test"
 server = Server(
     DemoModel,
     prefix,
-    model_kwargs={"input_variables": input_variables, "output_variables": output_variables},
+    model_kwargs={
+        "input_variables": input_variables,
+        "output_variables": output_variables,
+    },
     read_only=True,
-    protocols=["pva"]
-
+    protocols=["pva"],
 )
 # monitor = False does not loop in main thread
 server.start(monitor=True)
