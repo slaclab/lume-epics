f8874f1c3171c0ed01dbca3a71727c141f0bc060
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-01-05 15:33:06 -0800

    Refactor examples

--- examples/server.py
+++ examples/server.py
@@ -1,16 +1,23 @@
-from examples.model import DemoModel
 from lume_epics.epics_server import Server
 from lume_model.utils import variables_from_yaml
+from lume_epics.utils import config_from_yaml
+import os
+from examples.model import DemoModel
 
-with open("examples/files/demo_config.yml", "r") as f:
-    input_variables, output_variables = variables_from_yaml(f)
+import logging
 
-prefix = "test"
 
 if __name__ == "__main__":
+    with open("examples/files/demo_config.yml", "r") as f:
+        input_variables, output_variables = variables_from_yaml(f)
+
+    with open("examples/files/epics_config.yml", "r") as f:
+        epics_config = config_from_yaml(f)
+
+    prefix = "test"
     server = Server(
         DemoModel,
-        prefix,
+        epics_config,
         model_kwargs={
             "input_variables": input_variables,
             "output_variables": output_variables,
