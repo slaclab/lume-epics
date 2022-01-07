20c0dc01c255846d0fdfbfd929b73b3f274728c2
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-01-07 14:26:49 -0800

    Correct serving command

--- lume_epics/commands/serve_from_template.py
+++ lume_epics/commands/serve_from_template.py
@@ -7,9 +7,7 @@ from lume_epics.utils import config_from_yaml
 @click.command()
 @click.argument("filename")
 @click.argument("epics_config_filename")
-@click.option("--serve-ca", type=bool, default=True)
-@click.option("--serve-pva", type=bool, default=True)
-def serve_from_template(filename, epics_config_filename, serve_ca, serve_pva):
+def serve_from_template(filename, epics_config_filename):
 
     with open(filename, "r") as f:
         model_class, model_kwargs = model_from_yaml(f, load_model=False)
