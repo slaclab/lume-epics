11cef84006715c0b2fb863b2a83c230cf543d387
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-05-24 09:29:21 -0700

    Fix imports for model

--- examples/model.py
+++ examples/model.py
@@ -1,19 +1,23 @@
 import numpy as np
-from lume_model.variables import ScalarInputVariable, ImageOutputVariable, ScalarOutputVariable
-from lume_model.models import SurrogateModel
-from lume_model.utils import save_variables
+from lume_model.variables import (
+    ScalarInputVariable,
+    ImageOutputVariable,
+    ScalarOutputVariable,
+)
+from lume_model.models import BaseModel
 
-class DemoModel(SurrogateModel):
-    def __init__(self, input_variables=None, output_variables=None):
+
+class DemoModel(BaseModel):
+    def __init__(self):
         self.input_variables = input_variables
         self.output_variables = output_variables
 
     def evaluate(self, input_variables):
         input_variables = {input_var.name: input_var for input_var in input_variables}
         self.output_variables["output1"].value = np.random.uniform(
-            input_variables["input1"].value, # lower dist bound
-            input_variables["input2"].value, # upper dist bound
-            (50,50)
+            input_variables["input1"].value,  # lower dist bound
+            input_variables["input2"].value,  # upper dist bound
+            (50, 50),
         )
         self.output_variables["output2"].value = input_variables["input1"].value
         self.output_variables["output3"].value = input_variables["input2"].value
