69e1c19a66b0bece27df564e26904d0505650462
craftablescience <me@craftablescience.info>
2025-05-21 17:13:54 -0700

    feat: modern buildsystem, pypi publish action

--- lume_epics/__init__.py
+++ lume_epics/__init__.py
@@ -2,11 +2,6 @@ import os
 
 os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "1000000"
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
-
 # EPICS variables
 # channel access
 CA_VARS = [
